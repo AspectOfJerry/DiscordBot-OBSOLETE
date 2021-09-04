36bc7d1199e47a727de205891a22472ba3ae3bac
Jerry <jerryfu_mc@outlook.com>
2021-09-04 14:06:28 -0400

    Minor changes

--- commands/temp.js
+++ commands/temp.js
@@ -1,6 +1,5 @@
 module.exports = {
-    name: 'ping',
-    aliases: ['call'],
+    name: 'temp',
     description: 'Usage: "%ping"',
     execute(message, args, cmd, client, Discord){
         message.channel.reply('temp.js')
