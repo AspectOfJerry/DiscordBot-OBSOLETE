d9bc9c015ee5c2cb5dee1d718568c710db0a4064
AspectOfJerry <jerryfu_mc@outlook.com>
2021-10-11 10:42:26 -0400

    Removed join message

--- main.js
+++ main.js
@@ -16,25 +16,9 @@ client.events = new Discord.Collection();
 client.on('guildMemberAdd', guildMember => {
     let welcomeRole = guildMember.guild.roles.cache.get('890065114865238018')
     guildMember.roles.add(welcomeRole).catch(console.error);
-
-    const userJoinMessage = new Discord.MessageEmbed()
-        .setColor('#006400')
-        .setTitle('User join')
-        .setDescription(`<@${guildMember.user.id}> joined the server!`)
-        .setImage(`${guildMember.user.displayAvatarURL({dynamic: true, size: 128})}`)
-        .setFooter('Welcome!')
-
-    guildMember.guild.channels.cache.get('890067108287873094').send(userJoinMessage)
 });
 
 client.on('guildMemberRemove', guildMember => {
-    const userLeaveMessage = new Discord.MessageEmbed()
-        .setColor('#8b0000')
-        .setTitle('User leave')
-        .setDescription(`<@${guildMember.user.id}> left the server!`)
-        .setImage(`${guildMember.user.displayAvatarURL({dynamic: true, size: 128})}`)
-        .setFooter('Goodbye!')
-
     guildMember.guild.channels.cache.get('890067108287873094').send(userLeaveMessage)
 });
 
