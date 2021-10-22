c8ed67817425ab556498f5f489446ddd1fd0979e
Jerry <jerryfu_mc@outlook.com>
2021-10-21 20:31:08 -0400

    Installed new packages

---
+++ commands/stats.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'stats',
+    description: 'Usage: "%stats"',
+    execute(message, args, cmd, client, Discord) {
+        if(message.content == 'stats'){
+            
+        }
+    }
+}
