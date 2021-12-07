b72ce2c2dabd51f41a35c3bb064f923e195826e4
Jerry <jerryfu_mc@outlook.com>
2021-12-06 20:03:08 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,11 +2,21 @@ module.exports = {
     name: 'ping',
     description: 'Usage: "%ping"',
     execute(message, args, cmd, client, Discord) {
-        const ping = new Discord.MessageEmbed()
-            .setColor('#7dc8cd')
+        let pong;
+        const sendingPing = new Discord.MessageEmbed()
+            .setColor('#ffff00')
             .setThumbnail(`${message.author.displayAvatarURL({dynamic: true, size: 16})}`)
-            .setDescription('pong')
+            .setDescription('sending ping...')
 
-        message.channel.send(ping);
+        message.channel.send(sendingPing).then(resultMessage => {
+            pong = new Discord.MessageEmbed()
+                .setColor('#7dc8cd')
+                .setThumbnail(`${message.author.displayAvatarURL({dynamic: true, size: 16})}`)
+                .addField(`Bot latency`, `${resultMessage.createdTimestamp - message.createdTimestamp}ms`, true)
+                .addField(`API latency`, `${client.ws.ping}ms`, true)
+
+            message.channel.bulkDelete(1).catch(console.error);
+            message.channel.send(pong)
+        })
     }
 }
