06f9334dd0be804db81086a2e14a64dbacea26d6
Jerry <jerryfu_mc@outlook.com>
2022-02-07 14:40:18 -0500

    Updated say.js

--- commands/say.js
+++ commands/say.js
@@ -2,14 +2,28 @@ module.exports = {
     name: 'say',
     description: 'Usage: "%say <message>"',
     execute(message, args, cmd, client, Discord) {
+        //?
+        if(args[0] == '?') {
+            const helpCommand = new Discord.MessageEmbed()
+                .setColor('0000ff')
+                .setThumbnail(`${message.author.displayAvatarURL({dynamic: true, size: 32})}`)
+                .setTitle('%say command help')
+                .setDescription('This command sends a message in the current channel.')
+                .addField('Usage', '`%say` `<message>`', false)
+                .addField('Related commands', '`%send`')
+                .setFooter('This command is not case-sensitive.')
+
+            message.channel.send(helpCommand)
+            return;
+        }
+        //Declaring variables
         let messageSend
-
+        const requireArgs1 = new Discord.MessageEmbed()
+            .setColor('#ff0000')
+            .setThumbnail(`${message.author.displayAvatarURL({dynamic: true, size: 16})}`)
+            .setDescription('You must enter a message to say')
+        //Code
         if(!args[0]) {
-            const requireArgs1 = new Discord.MessageEmbed()
-                .setColor('#ff0000')
-                .setThumbnail(`${message.author.displayAvatarURL({dynamic: true, size: 16})}`)
-                .setDescription('You must enter a message to say')
-
             message.channel.send(requireArgs1)
         } else {
             messageSend = args.join(" ")
