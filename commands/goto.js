9796645cbee7f7d5630703237808aa1600e88605
Jerry <jerryfu_mc@outlook.com>
2022-02-07 14:49:23 -0500

    Updated goto.js

---
+++ commands/goto.js
@@ -0,0 +1,44 @@
+module.exports = {
+    name: 'goto',
+    description: 'Usage: "%goto <#channel> (<@user>)"',
+    aliases: ['go', 'switch', 'move'],
+    execute(message, args, cmd, client, Discord) {
+        //?
+        if(args[0] == '?') {
+            const helpCommand = new Discord.MessageEmbed()
+                .setColor('0000ff')
+                .setTitle('%return command help')
+                .setDescription('This command moves you to one of the Public voice channels (default is 0)')
+                .addField('Usage', "%return (`<0, 1, 2>`) (`<@user, all>`)")
+                .addField('Aliases', "`ret`, `r`")
+                .setFooter('This command is not case-sensitive.')
+
+            message.channel.send(helpCommand)
+            return;
+        }
+        //Declaring variables
+        const errorArgs0 = new Discord.MessageEmbed()
+            .setColor('ff0000')
+            .setThumbnail(`${message.author.displayAvatarURL({dynamic: true, size: 16})}`)
+            .setDescription('error: You must enter a valid Public channel number: `0`, `1`, `2`.')
+        const errorMustBeInVC = new Discord.MessageEmbed()
+            .setColor('ff0000')
+            .setThumbnail(`${message.author.displayAvatarURL({dynamic: true, size: 16})}`)
+            .setDescription('error: You must be in a voice channel to use this command')
+        const errorTarget = new Discord.MessageEmbed()
+            .setColor('#ff0000')
+            .setThumbnail(`${message.author.displayAvatarURL({dynamic: true, size: 16})}`)
+            .setDescription('Unknown user')
+        const errorTargetMustBeInVC = new Discord.MessageEmbed()
+            .setColor('ff0000')
+            .setThumbnail(`${message.author.displayAvatarURL({dynamic: true, size: 16})}`)
+            .setDescription('error: The target must be in a voice channel for you to use this command')
+        const successMoveTo = new Discord.MessageEmbed()
+            .setColor('00ff00')
+            .setThumbnail(`${message.author.displayAvatarURL({dynamic: true, size: 16})}`)
+            .setDescription(`Moved <@${message.member.user.id}> to [VOICE_CHANNEL].`)
+        //Code
+
+        
+    }
+}
